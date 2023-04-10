9d1ae171d21b2cf02ef27e90ca0163fb51462213
Mica Savournin <micaasavoo@gmail.com>
2023-04-10 16:36:17 -0300

    creamos schemas beats, userBuyer, userCreator

---
+++ api/src/schemas/index.js
@@ -0,0 +1,7 @@
+const models = {
+    UserCreatorModel: require('./nosql/userCreator'),
+    UserBuyerModel: require('./nosql/userBuyer'),
+    BeatsModel: require('./nosql/beats')
+
+}
+module.exports = models
---
+++ api/src/schemas/nosql/userCreator.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userCreatorSchema = mongoose.Schema({
+id:{
+    type: mongoose.Types.ObjectId
+},
+firstName:{
+    type: String,
+    require: true,
+},
+lastName:{
+    type:String,
+    require: true
+},
+username: {
+    type: String,
+    require: true,
+    unique: true
+},
+email: {
+    type: String,
+    require: true,
+    unique: true
+},
+password: {
+    type: String,
+    require: true
+},
+image:{
+    type: String,
+    require: false,
+    default: "https://static.vecteezy.com/system/resources/previews/009/734/564/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg"
+},
+createdBeats: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Beat'
+}]
+});
+
+module.exports = mongoose.model('UserCreator', userCreatorSchema);
