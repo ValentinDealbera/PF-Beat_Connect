0afde5c8525c47ea6c671d811e1fae47417d8e06
Valentin Dealbera <valentindealbera01@gmail.com>
2023-04-10 12:15:32 -0300

    Redux config (possible bugs)

---
+++ client/src/components/layout/header.js
@@ -0,0 +1,7 @@
+export default function Header () {
+    return (
+        <>
+        <h1 className="" >im the header</h1>
+        </>
+    )
+}
--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.scss'
+import { Header } from '@/components'
+import { Provider } from 'react-redux'
+import store,{persistor} from '@/redux/store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+     <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+    <Header/>
+    <Component {...pageProps} />
+    </PersistGate>
+      </Provider>
+    </>
+    )
 }
