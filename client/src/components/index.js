01754520e9c38ce3a5400c8bd73c6e55288a56b7
unknown <thomasbarenghi@gmail.com>
2023-04-10 10:21:20 -0300

    schema

---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+// // Importamos y exportamos los componentes de la carpeta "clients"
+// export { default as CartProductBox } from './clients/cart/CartProductBox';
+// export { default as MiniCart } from './clients/cart/MiniCart';
+// export { default as MiniCartItem } from './clients/cart/MiniCartItem';
+// export { default as CategoryItem } from './clients/category/CategoryItem';
+// export { default as ProductImage } from './clients/product/ProductImage';
+// export { default as ProductItem } from './clients/product/ProductItem';
+// export { default as ProductTextBox } from './clients/product/ProductTextBox';
+// export { default as ProductTotal } from './clients/product/ProductTotal';
+// export { default as QuantityManager } from './clients/product/QuantityManager';
