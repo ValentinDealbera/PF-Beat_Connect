7891a6fe9717635f5ab6211618d82fa74598ce85
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-04-11 10:32:26 -0300

    beats grid, bug inicio

--- client/src/components/client/beatCard.js
+++ client/src/components/client/beatCard.js
@@ -1,21 +1,32 @@
-import Image from "next/image"
-import Foto from '../../../public/images/matthew-moloney-tKB1GDJUq9c-unsplash.jpg'
+import Image from "next/image";
+import Foto from "../../../public/images/matthew-moloney-tKB1GDJUq9c-unsplash.jpg";
 
-export default function BeatCard ({beat, height, width}) {
-    return (
-        <>
-        <div className='flex flex-col background-neutral-white gap-estilo3'>
-            <div>
-            <Image className="aspect-square border-radius-estilo2" src={Foto} alt={"No No No"} />
-            </div>
-            <div>
-            <span className="font-semibold color-primary-red-700">{`$${'29.99'}`}</span><span className="font-light">{` | ${'100'}BPM`}</span>
-            <h1 className="font-bold">{`${'No No No - $50 UNLIM'}`}</h1>
-            <div className="flex flex-row items-center gap-1">
-            <span className="font-light">{`${'Example Author'}`}</span><Image className="inline" width={14} height={14} src={'/icon/checked-blue.svg'}/>
-            </div>
-            </div>
+export default function BeatCard({ beat, height, width }) {
+  return (
+    <>
+      <div className="background-neutral-white gap-estilo3 flex flex-col">
+        <div>
+          <Image
+            className="border-radius-estilo2 aspect-square"
+            src={Foto}
+            alt={"No No No"}
+          />
         </div>
-        </>
-    )
+        <div>
+          <span className="color-primary-red-700 font-semibold">{`$${"29.99"}`}</span>
+          <span className="font-light">{` | ${"100"}BPM`}</span>
+          <h1 className="font-bold">{`${"No No No - $50 UNLIM"}`}</h1>
+          <div className="flex flex-row items-center gap-1">
+            <span className="font-light">{`${"Example Author"}`}</span>
+            <Image
+              className="inline"
+              width={14}
+              height={14}
+              src={"/icon/checked-blue.svg"}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- client/src/pages/beats/index.js
+++ client/src/pages/beats/index.js
@@ -1,4 +1,11 @@
-import { Head, Main, Section, Hero, Search } from "@/components";
+import {
+  Head,
+  Main,
+  Section,
+  Hero,
+  Search,
+  BeatsShopSection,
+} from "@/components";
 
 export default function Beats() {
   return (
@@ -9,12 +16,16 @@ export default function Beats() {
           className="background-primary-red-700"
           style={{ minHeight: "20vh" }}
         >
-          <div className="padding-estilo2 flex h-full w-full flex-col gap-estilo3 items-start justify-between align-middle md:flex-row md:items-center">
+          <div className="padding-estilo2 gap-estilo3 flex h-full w-full flex-col items-start justify-between align-middle md:flex-row md:items-center">
             <h1 className="text-titulo1-semibold text-white">Explore Tracks</h1>
-            <Search colorMode="light" sizeMode="long" className={"w-full md:w-max"} />
+            <Search
+              colorMode="light"
+              sizeMode="long"
+              className={"w-full md:w-max"}
+            />
           </div>
         </Hero>
-        <Section subClassName="padding-estilo2"></Section>
+        <BeatsShopSection />
       </Main>
     </>
   );
