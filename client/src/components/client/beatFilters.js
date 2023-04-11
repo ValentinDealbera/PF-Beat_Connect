19f57829117665fee82008b6f8d6b8c8dbab2879
Valentin Dealbera <valentindealbera01@gmail.com>
2023-04-11 20:55:49 -0300

    Filters: style_done, console errors cleaned

--- client/src/components/client/beatFilters.js
+++ client/src/components/client/beatFilters.js
@@ -7,12 +7,14 @@ export default function BeatFilters() {
     const [beatTypes, setBeatTypes] = useState([])
     const [prices, setPrices] = useState({min:0, max:0, filter: false})
     const [BPM, setBPM] = useState({min:0, max:0, filter: false})
+    const [sort, setSort] = useState('')
     useEffect(()=>{
         console.log(beatGenre);
         console.log(beatTypes);
         console.log(prices);
         console.log(BPM);
-    },[BPM, prices, beatTypes, beatGenre])
+        console.log(sort);
+    },[BPM, prices, beatTypes, beatGenre,sort])
     const generos = genres.map(e=>{
         return {
             value: e.name,
@@ -20,7 +22,9 @@ export default function BeatFilters() {
         }
     })
     const types = [{value: 'VOCAL', label: 'VOCAL'},{value: 'BEAT', label: 'BEAT'},{value: 'SONG', label: 'SONG'},]
+    const sortArr = [{value: 'Price ↑', label: 'Price ↑'},{value: 'Price ↓', label: 'Price ↓'},{value: 'BPM ↑', label: 'BPM ↑'},{value: 'BPM ↓', label: 'BPM ↓'},{value: 'A - Z', label: 'A - Z'},{value: 'Z - A', label: 'Z - A'},]
   return (
+    <div className="flex flex-row justify-between">
     <div className="flex flex-row gap-estilo3">
         <div>
     <Select type='multiSelect' label='Genre ⌄' valores={generos} setSeleccionados={setBeatGenre} seleccionados={beatGenre} />
@@ -33,6 +37,10 @@ export default function BeatFilters() {
     </div>
     <div>
     <Select type='prices' label='BPM' setSeleccionados={setBPM} seleccionados={BPM} />
+    </div>
+    </div>
+    <div>
+    <Select label='Sort By' valores={sortArr} setSeleccionados={setSort} seleccionados={sort} />
     </div>
     </div>
   )
