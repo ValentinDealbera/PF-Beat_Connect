bab3ee390892affce57ea000580d6279e2d4e6dd
CarlaFrias <carliiitafriiias@gmail.com>
2023-04-14 16:06:12 -0300

    modifico genre

--- api/src/routes/genre.js
+++ api/src/routes/genre.js
@@ -1,17 +1,41 @@
 const express = require("express");
 const router = express();
-const {OK, BAD_REQUEST} =  require('../controllers/status')
-const getGenresController = require('../controllers/genreController')
+const {OK, BAD_REQUEST, GENRE_NOT_FOUND} =  require('../controllers/status')
+const {getGenresController, getGenreId} = require('../controllers/genreController')
+const GenreModel = require("../models/nosql/genre")
 
 router.get("/", async (req, res) => {
 
-      const genres = await getGenresController();
-
     try {
+      const genres = await getGenresController();
       res.status(OK).json(genres);
     } catch (error) {
       res.status(BAD_REQUEST).json({ message: error.message });
     }
   });
 
+
+  router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const genreId = await getGenreId(id);
+      genreId
+        ? res.status(OK).send(genreId)
+        : res.status(NOT_FOUND).send(GENRE_NOT_FOUND);
+    } catch (err) {
+      res.status(NOT_FOUND).send(GENRE_NOT_FOUND);
+    }
+  });
+
+  router.post("/", async (req, res) => {
+    const {body} = req;
+  
+    try {
+      const genre = await GenreModel.create(body);
+      res.send(genre);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+  
 module.exports = router;
