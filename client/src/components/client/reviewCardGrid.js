cc2dc49c3424cb951169003946ad0273841da1b6
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-04-25 18:45:16 -0300

    reviews etapa 1

--- client/src/components/client/reviewCardGrid.js
+++ client/src/components/client/reviewCardGrid.js
@@ -1,52 +1,57 @@
 import {
-    BeatCard,
-    BeatRightSheet,
-    BeatDetailSideBar,
-    BeatBottomSheet,
-    Loader,
-  } from "@/components";
-  import { useState } from "react";
-  import { useSelector } from "react-redux";
-  
-  export function externalManageDropdown() {
-    // NewBeatCardGrid.handleDropdownOpen();
-  }
-  
-  export default function ReviewCardGrid(props) {
+  BeatCard,
+  BeatRightSheet,
+  BeatDetailSideBar,
+  BeatBottomSheet,
+  Loader,
+  ClientReview,
+} from "@/components";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
-    const [isDropDown, setIsDropdownOpen] = useState(false);
-    const {reviews} = useSelector((state) => state.client.reviews);
-  
+export function externalManageDropdown() {
+  // NewBeatCardGrid.handleDropdownOpen();
+}
 
-    return (
-      <>
-        <div className="gap-estilo1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5">
-          {props.beats && props.beats.length > 0 && (
-            <>
-              {reviews?.map((reviews) => (
-                <>
+export default function ReviewCardGrid(props) {
+  const [isDropDown, setIsDropdownOpen] = useState(false);
+  const reviews = useSelector((state) => state.client.reviews.reviews);
 
-                  
-                </>
-              ))}
-            </>
-          )}
-        </div>
-        {isDropDown && (
+  console.log(reviews);
+
+  return (
+    <>
+      <div className="gap-estilo1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5">
+        {reviews && reviews.length > 0 && (
           <>
-            <div className="hidden sm:flex">
-              <BeatRightSheet setIsDropdownOpen={setIsDropdownOpen}>
-                <BeatDetailSideBar />
-              </BeatRightSheet>
-            </div>
-            <div className="flex sm:hidden">
-              <BeatBottomSheet setIsDropdownOpen={setIsDropdownOpen}>
-                <BeatDetailSideBar />
-              </BeatBottomSheet>
-            </div>
+            {reviews?.map((review) => (
+              <>
+                <ClientReview
+                  currentMode=""
+                  title={review.title}
+                  comment={review.comment}
+                  username={`${review.createdBy.firstName} ${review.createdBy.lastName}`}
+                  review={review}
+                />
+              </>
+            ))}
           </>
         )}
-      </>
-    );
-  }
-  
+      </div>
+      {isDropDown && (
+        <>
+          <div className="hidden sm:flex">
+            <BeatRightSheet setIsDropdownOpen={setIsDropdownOpen}>
+              <BeatDetailSideBar />
+            </BeatRightSheet>
+          </div>
+          <div className="flex sm:hidden">
+            <BeatBottomSheet setIsDropdownOpen={setIsDropdownOpen}>
+              <BeatDetailSideBar />
+            </BeatBottomSheet>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
