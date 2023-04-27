2b1946b3dd244ffda6b99c97acf7c598761d90dc
Tadeo <massotadeo@gmail.com>
2023-04-27 16:32:24 -0300

    dark mode base

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -3,9 +3,6 @@ import { Header, Footer, Master, HOC } from "@/components";
 import { Provider, useDispatch } from "react-redux";
 import store, { persistor } from "@/redux/store/store";
 import { PersistGate } from "redux-persist/integration/react";
-
-import { ThemeProvider } from 'next-themes';
-
 import { Toaster, toast } from "sonner";
 import { I18nextProvider } from "react-i18next";
 import { ThemeProvider } from 'next-themes';
@@ -46,15 +43,7 @@ export default function App({ Component, pageProps, router }) {
         <PersistGate loading={null} persistor={persistor}>
           <HOC>
 
-          <ThemeProvider attribute="class">
-          {headerVisibility && <Header />}
-          <Master>
-          <Toaster position="bottom-left" />
-          <Component {...pageProps} />
-          </Master>
-          {headerVisibility && <Footer mode={mode} />}
-        
-
+          
             <ThemeProvider attribute="class">
             <I18nextProvider i18n={i18next}>
               {headerVisibility && <Header />}
