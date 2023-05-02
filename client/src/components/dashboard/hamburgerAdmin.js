bcfb63bc2334478eb87dfe3e061d2f93e6e0b9ad
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-05-01 22:12:47 -0300

    hamburguer admin, bugs

--- client/src/components/dashboard/hamburgerAdmin.js
+++ client/src/components/dashboard/hamburgerAdmin.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 import { useState } from "react";
 import NavModalItem from "../layout/nav/navModalItem";
 import VerticalNav from "../layout/nav/verticalNav";
-import { navHelp,navClient } from "@/data/data";
+import { navHelp, navClient } from "@/data/data";
 import { UserBoxNav } from "@/components";
 
 export default function HamburgerAdmin({ options, manageHamburguer }) {
   const [t, i18n] = useTranslation("global");
-  console.log(options)
+  console.log(options);
   return (
     <>
       <div
@@ -19,7 +19,7 @@ export default function HamburgerAdmin({ options, manageHamburguer }) {
           top: 0,
         }}
       >
-        <div className=" absolute left-0 top-0 z-40 flex w-screen items-center justify-between px-4 xs:px-8 py-6 ">
+        <div className=" absolute left-0 top-0 z-40 flex w-screen items-center justify-between px-4 py-6 xs:px-8 ">
           <img
             onClick={() => {
               manageHamburguer(false);
@@ -31,22 +31,21 @@ export default function HamburgerAdmin({ options, manageHamburguer }) {
         </div>
 
         <div className="flex h-full flex-col items-start justify-center gap-1 px-4 py-16 xs:px-8">
-            <div className="flex flex-col gap-4 ">
-              {options.map((item, index) => (
-                <Link
-                  href={item.url}
-                  key={index}
-                  onClick={() => manageHamburguer(false)}
-                >
-                  <div className="cursor-pointer">
-                    <h1 className="text-titulo1-medium text-white">
-                      {t(item.title)}
-                    </h1>
-                  </div>
-                </Link>
-              ))}
-            </div>
-   
+          <div className="flex flex-col gap-4 ">
+            {options.map((item, index) => (
+              <Link
+                href={item.url}
+                key={index}
+                onClick={() => manageHamburguer(false)}
+              >
+                <div className="cursor-pointer">
+                  <h1 className="text-titulo1-medium text-white">
+                    {t(item.title)}
+                  </h1>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </>
