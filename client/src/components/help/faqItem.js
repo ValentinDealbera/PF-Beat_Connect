691fc894d1bf9d0f9ba4f2bf79d0a6d198baeca7
Tadeo <massotadeo@gmail.com>
2023-05-02 18:50:13 -0300

    bug dark mode

--- client/src/components/help/faqItem.js
+++ client/src/components/help/faqItem.js
@@ -1,15 +1,21 @@
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
 import { ReactSVG } from "react-svg";
+import { useRouter } from "next/router";
 
 export default function FaqsItem({ faq }) {
+  const router = useRouter();
   const [t, i18n] = useTranslation("global");
   return (
     <div className="faq-item gap-estilo4 flex flex-col">
       <div className="gap-estilo4 flex flex-row  items-center">
         <ReactSVG
-          src="/icon/faq-icon.svg"
-          className="dashboard-item__icon h-[24px] w-[24px] fill-current text-black dark:text-white"
+          src={
+            router.pathname.startsWith("/help")
+              ? "/icon/dashboard/faq-dark.svg"
+              : "/icon/dashboard/faq-icon.svg"
+          }
+          className="dashboard-item__icon h-[24px] w-[24px] fill-current text-black dark:text-dark"
         />
         <h3 className="text-subtitulo-medium">{t(faq.title)}</h3>
       </div>
